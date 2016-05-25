91494c7542c37c781f5abbbc3c0cb469a849becf
David Turner <susbdt@gmail.com>
2016-05-25 07:31:19 -0700

    X

--- experiments/pixels/main.cpp
+++ experiments/pixels/main.cpp
@@ -13,9 +13,26 @@ SDL_Window * window;
 SDL_Surface * surface;
 SDL_Renderer * renderer;
 
-double prob = .03;
+// x: x position as a percent of window width
+// y: y position as a percent of window height
+// radius: radius as a percent of window width
+// p: density as a probability
+void drawDisk(double x, double y, double radius, char r, char g, char b, double p) {
+	for (int i = 0; i < surface->w; ++i) {
+		for (int j = 0; j < surface->h; ++j) {
+			double dist = sqrt((surface->w * x - i) * (surface->w * x - i) + (surface->h * y - j) * (surface->h * y - j));
+			double prob = 0;
+			double radiusInPixels = radius * surface->w;
+			if (dist < radiusInPixels) prob = (radiusInPixels - dist) / radiusInPixels * p;
+			if (Util::randomBool(prob)) {
+				SDL_SetRenderDrawColor(renderer, r, g, b, 255);
+				SDL_RenderDrawPoint(renderer, i, j);
+			}
+		}
+	}
+}
 
-void draw() {
+void drawPixels(double prob) {
 	for (int i = 0; i < surface->w; ++i) {
 		for (int j = 0; j < surface->h; ++j) {
 			if (Util::randomBool(prob)) {
@@ -30,7 +47,6 @@ void draw() {
 			}
 		}
 	}
-	SDL_UpdateWindowSurface(window);
 }
 
 bool processInput() {
@@ -48,6 +64,7 @@ bool processInput() {
 
 int main(int argc, char* argv[]) {
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) return 1;
+	SDL_ShowCursor(SDL_DISABLE);
 	SDL_DisplayMode display;
 	assert(SDL_GetCurrentDisplayMode(0, &display) == 0);
 	window = SDL_CreateWindow("Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, display.w, display.h, SDL_WINDOW_FULLSCREEN);
@@ -56,8 +73,11 @@ int main(int argc, char* argv[]) {
 	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 	SDL_RenderClear(renderer);
 
+	drawPixels(1);
+	drawDisk(.5, .5, .2, 0, 255, 0, 1.0);
 	while (processInput()) {
-		draw();
+		drawPixels(.03);
+		SDL_UpdateWindowSurface(window);
 		SDL_Delay(50);
 	}
 
